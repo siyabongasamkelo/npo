e63cb9c2d606251996dd3ea3d46e5abc6a042462
siyabonga <siyabongasamkelociam@gmail.com>
2024-10-01 17:41:02 +0200

    feat: made header responsive to mobile phone

--- client/organization/src/components/Header.js
+++ client/organization/src/components/Header.js
@@ -2,19 +2,38 @@ import React from "react";
 import { HeaderCover, HeaderWrapper, Links } from "./Header.styled";
 import { Link } from "react-router-dom";
 import { Button } from "./common/Button.styled";
-
+import { List } from "react-bootstrap-icons";
+import { useState } from "react";
+import HeaderSlider from "./HeaderSlider";
 const Header = () => {
+  const [closeMenu, setCloseMenu] = useState(true);
+  const [display, setDisplay] = useState("block");
+
   return (
     <HeaderCover>
       <HeaderWrapper>
         <h3>organization</h3>
         <Links>
           <Link to="/">Home</Link>
-          <Link to="/">Home</Link>
-          <Link to="/">Home</Link>
+          <Link to="/">About Us</Link>
+          <Link to="/">Our Mission</Link>
+          <Link to="/">Contact us</Link>
         </Links>
         <Button>Contact us</Button>
+        <List
+          onClick={() => {
+            closeMenu ? setDisplay("none") : setDisplay("block");
+            setCloseMenu(false);
+          }}
+        />
       </HeaderWrapper>
+      <HeaderSlider
+        setCloseMenu={() => {
+          setCloseMenu(true);
+        }}
+        closeMenu={closeMenu}
+        display={display}
+      ></HeaderSlider>
     </HeaderCover>
   );
 };
