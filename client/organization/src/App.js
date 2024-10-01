d9e5309b1ca071dacf72f02bfc1cc0dc9402390f
siyabonga <siyabongasamkelociam@gmail.com>
2024-10-01 12:26:29 +0200

    configuring react-router-dom

--- client/organization/src/App.js
+++ client/organization/src/App.js
@@ -1,9 +1,38 @@
 import { Container } from "react-bootstrap";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// function App() {
+//   return (
+//     <div className="App">
+//       <HomePage />
+//     </div>
+//   );
+// }
+
+// export default App;
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route index element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/*" element={<HomePage />} />
+      </Route>
+    )
+  );
   return (
-    <div className="App">
-      <Container></Container>
-    </div>
+    <>
+      <div>
+        <RouterProvider router={router} />
+      </div>
+    </>
   );
 }
 
