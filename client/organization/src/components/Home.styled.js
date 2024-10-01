c5988f3f9a040552266a04b3c99155d05a9687b5
siyabonga <siyabongasamkelociam@gmail.com>
2024-10-01 12:29:43 +0200

    few styles for home page

--- client/organization/src/components/Home.styled.js
+++ client/organization/src/components/Home.styled.js
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const HomeCover = styled.div`
   height: 100vh;
   width: 100%;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7)),
+    url(${(props) => props.backgroundImage});
+  background-size: cover;
+  background-position: center;
 `;
