a4b9c56471507a06fc7134458408aad0d602ff62
siyabonga <siyabongasamkelociam@gmail.com>
2024-10-01 17:42:51 +0200

    added social media icons to hero section

--- client/organization/src/components/Home.js
+++ client/organization/src/components/Home.js
@@ -1,12 +1,30 @@
 import React from "react";
-import { HomeCover } from "./Home.styled";
+import { HeroSection, HomeCover } from "./Home.styled";
 import background from "../assets/img/background.jpg";
 import Header from "./Header";
+import { Button } from "./common/Button.styled";
+import { Facebook, Whatsapp, Instagram, Twitter } from "react-bootstrap-icons";
 
 const Home = () => {
   return (
     <HomeCover backgroundImage={background}>
       <Header />
+      <HeroSection>
+        <h1>
+          Bringing Back The Lost <br></br>Hope{" "}
+        </h1>
+        <p>
+          we strive to bring back lost hope to all the kids <br></br>in our{" "}
+          <span>community</span>...
+        </p>
+        <Button>Contact Us</Button>
+        <div>
+          <Facebook />
+          <Whatsapp />
+          <Instagram />
+          <Twitter />
+        </div>
+      </HeroSection>
     </HomeCover>
   );
 };
--- client/organization/src/components/Home.styled.js
+++ client/organization/src/components/Home.styled.js
@@ -8,3 +8,40 @@ export const HomeCover = styled.div`
   background-size: cover;
   background-position: center;
 `;
+
+export const HeroSection = styled.div`
+  height: 60%;
+  width: 80%;
+  margin-left: 10%;
+  text-align: center;
+  h1 {
+    font-size: 84px;
+    color: rgba(255, 255, 255, 0.9);
+    margin-top: 15%;
+    letter-spacing: 10px;
+    line-height: 1.2;
+  }
+  p {
+    color: rgba(255, 255, 255, 0.9);
+    margin-top: -2%;
+  }
+  span {
+    color: #fff7d1;
+  }
+
+  div {
+    margin-top: 14%;
+    margin-left: -90%;
+  }
+
+  div svg {
+    transform: scale(150%);
+    color: rgba(255, 255, 255, 0.9);
+    margin-left: 40px;
+    &:hover {
+      color: #ffb0b0;
+      transition: 0.5s ease-in-out;
+      cursor: pointer;
+    }
+  }
+`;
