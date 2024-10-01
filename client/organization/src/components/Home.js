dd6ff6cde00950faa2f9eac3df8fb3cd8feb1436
siyabonga <siyabongasamkelociam@gmail.com>
2024-10-01 12:27:31 +0200

    added header to home page

--- client/organization/src/components/Home.js
+++ client/organization/src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { HomeCover } from "./Home.styled";
+import background from "../assets/img/background.jpg";
+import Header from "./Header";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <HomeCover backgroundImage={background}>
+      <Header />
+    </HomeCover>
+  );
 };
 
 export default Home;
