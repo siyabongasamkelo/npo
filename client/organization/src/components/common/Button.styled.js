41a7f413a2d8a7d487e43296669f387ee3ebf77c
siyabonga <siyabongasamkelociam@gmail.com>
2024-10-01 12:33:51 +0200

    file for all common javascript files

---
+++ client/organization/src/components/common/Button.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  height: 40px;
+  width: 150px;
+  background-color: #fff7d1;
+  border: none;
+  border-radius: 30px;
+  font-size: 14px;
+  cursor: pointer;
+  &:hover {
+    background-color: #ffb0b0;
+    transition: 0.5s ease-in-out;
+  }
+`;
