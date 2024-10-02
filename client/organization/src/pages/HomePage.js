4df3027a2eaf6dcd53975bd13beac3c734916eba
siyabonga <siyabongasamkelociam@gmail.com>
2024-10-02 14:58:44 +0200

    added container to make it apear better

--- client/organization/src/pages/HomePage.js
+++ client/organization/src/pages/HomePage.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Home from "../components/Home";
+import AboutUs from "../components/AboutUs";
+import { Container } from "react-bootstrap";
 
 const HomePage = () => {
   return (
     <div>
       <Home />
+      <Container>
+        <AboutUs />
+      </Container>
     </div>
   );
 };
