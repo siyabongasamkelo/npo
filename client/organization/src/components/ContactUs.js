93bf3b0f6c7e7ce0d91ef14e8e02599e9dcc1a67
siyabonga <siyabongasamkelociam@gmail.com>
2024-10-09 19:58:42 +0200

    contact us section and all its content

---
+++ client/organization/src/components/ContactUs.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  ContactDetails,
+  ContactFormCover,
+  ContactUsCover,
+  ContactUsWrapper,
+} from "./ContactUs.styled";
+import {
+  Envelope,
+  Facebook,
+  GeoAlt,
+  Instagram,
+  Telephone,
+  TwitterX,
+  Whatsapp,
+} from "react-bootstrap-icons";
+import ContactForm from "./common/ContactForm";
+
+const ContactUs = () => {
+  return (
+    <ContactUsCover>
+      <h2>Contact Us</h2>
+      <ContactUsWrapper>
+        <ContactFormCover>
+          <ContactForm />
+        </ContactFormCover>
+        <ContactDetails>
+          <p>
+            <Envelope /> siya@gmail.com
+          </p>
+          <p>
+            <Telephone /> 061 189 2221
+          </p>
+          <p>
+            <GeoAlt /> Kwazulu-Natal, Nquthu, Sandlwana
+          </p>
+          <p>
+            <Facebook />
+            <TwitterX />
+            <Instagram />
+            <Whatsapp />
+          </p>
+        </ContactDetails>
+      </ContactUsWrapper>
+    </ContactUsCover>
+  );
+};
+
+export default ContactUs;
