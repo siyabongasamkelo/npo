00dd90945152e4f783507600f6a4a41d85113a54
siyabonga <siyabongasamkelociam@gmail.com>
2024-11-15 14:30:59 +0200

    added new packages

---
+++ client/organization/src/components/Donate.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { DonateCover, DonateWrapper } from "./Donate.styled";
+import { Button } from "./common/Button.styled";
+
+const Donate = () => {
+  return (
+    <DonateCover>
+      <DonateWrapper>
+        <h1>Donate Any Amount Now</h1>
+        <p>Your donations can help us make a huge different...</p>
+        <div>
+          <form>
+            <input
+              type="number"
+              placeholder="Enter Amount"
+              required
+              name="amount"
+              id="amount"
+            />
+            <br></br>
+            <Button type="submit">Donate</Button>
+          </form>
+        </div>
+      </DonateWrapper>
+    </DonateCover>
+  );
+};
+
+export default Donate;
