d74b17f600bd61209c50b1b076acf6387921ec11
siyabonga <siyabongasamkelociam@gmail.com>
2024-11-18 11:26:02 +0200

    fixed responsiveness errors in contact page

--- client/organization/src/components/ContactUs.styled.js
+++ client/organization/src/components/ContactUs.styled.js
@@ -15,9 +15,36 @@ export const ContactUsWrapper = styled.div`
 
 export const ContactDetails = styled.div`
   font-family: "Afacad Flux", sans-serif;
+
+  @media only screen and (max-width: 600px) {
+    width: 80%;
+    margin-left: 10%;
+    svg {
+      padding-left: 15px;
+    }
+  }
+
+  @media only screen and (min-width: 768px) {
+    width: 80%;
+    margin-left: 10%;
+    margin-top: 8%;
+    svg {
+      padding-left: 15px;
+    }
+    p {
+      font-size: 22px;
+    }
+  }
+
   @media only screen and (min-width: 992px) {
     height: 50vh;
     width: 30%;
+    margin-left: 0;
+    margin-top: 0%;
+
+    p {
+      font-size: 18px;
+    }
     svg {
       padding-left: 10px;
       cursor: pointer;
@@ -25,13 +52,6 @@ export const ContactDetails = styled.div`
         color: #ffb0b0;
         transition: 0.5s ease-in-out;
       }
-    }
-  }
-  @media only screen and (max-width: 600px) {
-    width: 80%;
-    margin-left: 10%;
-    svg {
-      padding-left: 15px;
     }
   }
 `;
