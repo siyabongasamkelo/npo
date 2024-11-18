e19a7531f2a8829a9d106c948a8cfa0e4cfc835a
siyabonga <siyabongasamkelociam@gmail.com>
2024-11-18 11:28:38 +0200

    fixed responsiveness errors in contact page

--- client/organization/src/components/Donate.styled.js
+++ client/organization/src/components/Donate.styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const DonateCover = styled.section`
-  height: 35vh;
+  height: 30vh;
   width: 100%;
   background-color: #343131;
   margin-top: 80px;
@@ -10,6 +10,55 @@ export const DonateCover = styled.section`
 export const DonateWrapper = styled.div`
   font-family: "Afacad Flux", sans-serif;
   color: #fff7d1;
+
+  @media only screen and (max-width: 600px) {
+    width: 90%;
+    margin: 5%;
+    input {
+      width: 95%;
+      height: 35px;
+      padding-left: 5%;
+      border: none;
+      border-radius: 30px;
+    }
+
+    button {
+      margin-top: 5%;
+      width: 100%;
+    }
+    h1 {
+      margin-top: 5%;
+    }
+  }
+
+  @media only screen and (min-width: 768px) {
+    width: 80%;
+    margin: 10%;
+    input {
+      width: 95%;
+      height: 50px;
+      padding-left: 5%;
+      border: none;
+      border-radius: 30px;
+      font-size: 18px;
+    }
+
+    button {
+      margin-top: 5%;
+      width: 100%;
+      height: 50px;
+      font-size: 18px;
+    }
+    p {
+      font-size: 20px;
+      margin-top: -5%;
+    }
+    h1 {
+      padding-top: 5%;
+      font-size: 38px;
+    }
+  }
+
   @media only screen and (min-width: 992px) {
     width: 80%;
     margin-left: 10%;
@@ -22,8 +71,8 @@ export const DonateWrapper = styled.div`
     }
 
     input {
-      height: 35px;
-      width: 20%;
+      height: 50px;
+      width: 25%;
       padding-left: 20px;
       border: none;
       border-radius: 30px;
@@ -32,17 +81,8 @@ export const DonateWrapper = styled.div`
     button {
       background-color: #ffb0b0;
       color: rgba(255, 255, 255, 0.9);
-      margin-top: 20px;
-    }
-  }
-
-  @media only screen and (max-width: 600px) {
-    width: 100%;
-    input {
-      width: 70%;
-      height: 35px;
-      border: none;
-      border-radius: 30px;
+      margin-top: -5%;
+      width: 15%;
     }
   }
 `;
