8a2a997cbf61bc3741a45ebbb807355e7e9253e5
siyabonga <siyabongasamkelociam@gmail.com>
2024-11-18 14:05:36 +0200

    removed useless code

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import crypto from "crypto";
-import { log } from "console";
 dotenv.config();
 
 const app = express();
