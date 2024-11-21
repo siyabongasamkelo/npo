18f4ee4229f43b731e321239478836411fdcb927
siyabonga <siyabongasamkelociam@gmail.com>
2024-11-21 11:03:12 +0200

    donations page

---
+++ client/organization/src/pages/DonationPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Donations from "../components/Donations";
+
+const DonationPage = () => {
+  return (
+    <div>
+      <Container>
+        <Donations />
+      </Container>
+    </div>
+  );
+};
+
+export default DonationPage;
