f7b2ab0ca07230d6ed5baaf5ba942aa9d148ee97
siyabonga <siyabongasamkelociam@gmail.com>
2024-11-22 12:43:29 +0200

    removing bootstrap container

--- client/organization/src/components/AboutUs.js
+++ client/organization/src/components/AboutUs.js
@@ -9,51 +9,46 @@ import {
   OurTeamDiv,
 } from "./AboutUs.styled";
 import aboutpic from "../assets/img/aboutpic.jpg";
-import { Container } from "react-bootstrap";
 import TeamCard from "./common/TeamCard";
 const AboutUs = () => {
   return (
-    <Container>
-      <AboutUsCover>
-        <AboutDiv>
-          <h2>About Us</h2>
-          <About>
-            <AboutPic>
-              <img src={aboutpic} alt="aboutpic" />
-            </AboutPic>
-            <AboutText>
-              <p>
-                As a non-profit organization located in Nquthu, our primary goal
-                is to foster the growth of young children into responsible,
-                successful adults. We are dedicated to providing free daily
-                meals to these children, enabling them to focus on their
-                education and personal development. Additionally, we support
-                young girls in local schools by donating sanitary pads and
-                offering motivational guidance to help them achieve their full
-                potential.
-                <br></br>
-                <br></br>
-                Our commitment to making a positive impact in rural areas drives
-                us to strive for a better future for all children. With our base
-                in Kwazulu Natal, Nquthu, we are proud to be a non-profit
-                organization actively making a tangible difference in the lives
-                of young individuals. Our documented efforts and impactful work
-                stand as a testament to our dedication and passion for this
-                cause.
-              </p>
-            </AboutText>
-          </About>
-        </AboutDiv>
-        <OurTeamDiv>
-          <h2>Our Team</h2>
-          <OurTeam>
-            <TeamCard></TeamCard>
-            <TeamCard></TeamCard>
-            <TeamCard></TeamCard>
-          </OurTeam>
-        </OurTeamDiv>
-      </AboutUsCover>
-    </Container>
+    <AboutUsCover>
+      <AboutDiv>
+        <h2>About Us</h2>
+        <About>
+          <AboutPic>
+            <img src={aboutpic} alt="aboutpic" />
+          </AboutPic>
+          <AboutText>
+            <p>
+              As a non-profit organization located in Nquthu, our primary goal
+              is to foster the growth of young children into responsible,
+              successful adults. We are dedicated to providing free daily meals
+              to these children, enabling them to focus on their education and
+              personal development. Additionally, we support young girls in
+              local schools by donating sanitary pads and offering motivational
+              guidance to help them achieve their full potential.
+              <br></br>
+              <br></br>
+              Our commitment to making a positive impact in rural areas drives
+              us to strive for a better future for all children. With our base
+              in Kwazulu Natal, Nquthu, we are proud to be a non-profit
+              organization actively making a tangible difference in the lives of
+              young individuals. Our documented efforts and impactful work stand
+              as a testament to our dedication and passion for this cause.
+            </p>
+          </AboutText>
+        </About>
+      </AboutDiv>
+      <OurTeamDiv>
+        <h2>Our Team</h2>
+        <OurTeam>
+          <TeamCard></TeamCard>
+          <TeamCard></TeamCard>
+          <TeamCard></TeamCard>
+        </OurTeam>
+      </OurTeamDiv>
+    </AboutUsCover>
   );
 };
 
