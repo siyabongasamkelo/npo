a851013b6968af48e9e22058454751994d821425
siyabonga <siyabongasamkelociam@gmail.com>
2024-11-22 14:39:56 +0200

    validations folder

---
+++ client/organization/src/validations/PersonalDetails.js
@@ -0,0 +1,7 @@
+import * as yup from "yup";
+
+export const personalDetails = yup.object().shape({
+  firstname: yup.string().required(),
+  lastname: yup.string().required(),
+  email: yup.string().email("email not valid").required(),
+});
