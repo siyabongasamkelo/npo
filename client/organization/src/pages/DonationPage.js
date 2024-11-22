5f06175cee5e3d1fccd8ac26bbeb98fb97090358
siyabonga <siyabongasamkelociam@gmail.com>
2024-11-22 14:39:05 +0200

    added footer to donations page

--- client/organization/src/pages/DonationPage.js
+++ client/organization/src/pages/DonationPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Donations from "../components/Donations";
+import Footer from "../components/common/Footer";
 
 const DonationPage = () => {
   return (
     <div>
       <Container>
         <Donations />
+        <Footer />
       </Container>
     </div>
   );
