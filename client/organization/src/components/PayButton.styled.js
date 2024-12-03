9d46618e2473c1b131afa5d3582035dc750eb010
siyabonga <siyabongasamkelociam@gmail.com>
2024-12-03 13:27:22 +0200

    paybutton styles

---
+++ client/organization/src/components/PayButton.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PayButtonWrapper = styled.section`
+  height: 100vh;
+  width: 100%;
+`;
+
+export const PayButtonDiv = styled.div`
+  p {
+    color: rgba(0, 0, 0, 0.8);
+    font-size: 16px;
+    margin-top: 2%;
+  }
+`;
