d6ac67b0b3b523b5582feb1b324df17517b946f8
siyabonga <siyabongasamkelociam@gmail.com>
2024-12-03 13:25:51 +0200

    pay button page

---
+++ client/organization/src/pages/PayButtonPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import PayButton from "../components/PayButton";
+
+const PayButtonPage = () => {
+  return (
+    <Container>
+      <PayButton />
+    </Container>
+  );
+};
+
+export default PayButtonPage;
