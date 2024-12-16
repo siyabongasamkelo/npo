675adc7a3208720f57f6ee954dd2709dae43c337
siyabonga <siyabongasamkelociam@gmail.com>
2024-12-16 23:29:42 +0200

    centering social medial icons

--- client/organization/src/components/common/Footer.styled.js
+++ client/organization/src/components/common/Footer.styled.js
@@ -34,6 +34,10 @@ export const Links = styled.div`
   @media only screen and (max-width: 600px) {
     text-align: center;
     margin-left: 0;
+    div {
+      display: flex;
+      justify-content: center;
+    }
   }
   @media only screen and (min-width: 768px) {
     text-align: center;
